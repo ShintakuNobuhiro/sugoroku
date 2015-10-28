e35dd658c7ae147ea7e101596df2e5dcf59d0e20
Shintaku <ukyankyan11@gmail.com>
2015-10-29 01:25:43 +0900

    no message

--- app/src/main/java/com/example/shintaku/myapplication/CanvasBasicView.java
+++ app/src/main/java/com/example/shintaku/myapplication/CanvasBasicView.java
@@ -3,10 +3,10 @@ package com.example.shintaku.myapplication;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.util.Log;
 import android.view.View;
 
 class CanvasBasicView extends View {
@@ -20,7 +20,7 @@ class CanvasBasicView extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         // Viewの描画サイズを指定する
-        setMeasuredDimension(10000, 1920);
+        setMeasuredDimension(100000, 1920);
     }
 
     @Override
@@ -33,9 +33,9 @@ class CanvasBasicView extends View {
         int sx = 400;
         int sy = 150;
         final int dy = 300;
-        final int recentcell = 5;
+        final int recentcell = 1;
         final int cell = 10;
-        canvas.translate(0, -dy * recentcell);
+
 
         //線路
         int stroke = 20;
@@ -43,6 +43,7 @@ class CanvasBasicView extends View {
         Rect rect = new Rect(x + sx / 2 - stroke, y, x + sx / 2 + stroke, y + height);
         int j = 0;
         mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        mPaint.setColor(Color.BLACK);
         while (height * j < 22 * dy) {
             if (j % 2 == 1) {
                 mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
@@ -60,29 +61,35 @@ class CanvasBasicView extends View {
         RectF rectf = new RectF(x, y, x + sx, y + sy);
         mPaint.setStyle(Paint.Style.STROKE);
         for (int i = 0; i < 22; i++) {
+            mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+            if(i <= recentcell){
+                mPaint.setColor(Color.GREEN);
+            } else if (i <= cell) {
+                mPaint.setColor(Color.YELLOW);
+            } else {
+                mPaint.setStyle(Paint.Style.STROKE);
+                mPaint.setColor(Color.BLACK);
+            }
             canvas.drawRoundRect(rectf, 30, 30, mPaint);
+            mPaint.setColor(Color.BLACK);
             canvas.drawText("新青森", x + sy / 2, (y + sy / 2) + dy * i, mPaint);
             rectf.offset(0, dy);
         }
 
+        canvas.translate(0, -dy * recentcell);
         //アニメーション(ちらつく)
-        final ValueAnimator anim = ValueAnimator.ofFloat(0, -1);
+        final ValueAnimator anim = ValueAnimator.ofFloat(0,5);
 // アニメーションの時間を設定する
-        anim.setDuration(1000);
+        anim.setDuration(100000);
         final float[] tmp = new float[1];
         anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 tmp[0] = (Float) (anim.getAnimatedValue());
-                canvas.translate(0, tmp[0]);
-                postInvalidate();
-                if (tmp[0] <= -dy * (cell - recentcell)) {
-                    Log.d("t", String.valueOf(tmp[0]));
-                    //anim.end();
-                }
+                canvas.translate(0, -2);
+                invalidate();
             }
         });
         anim.start();
-
     }
 }
--- app/src/main/java/com/example/shintaku/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shintaku/myapplication/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.shintaku.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new CanvasBasicView(this));
+        FrameLayout frameLayout = new FrameLayout(this);
+        setContentView(frameLayout);
+        frameLayout.addView(new CanvasBasicView(this));
     }
 }
